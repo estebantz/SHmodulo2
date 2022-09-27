88a1358b579deeb3e92b9c67c42deb0d06831243
Esteban Teran <esteban.teran400@gmail.com>
2022-09-27 12:10:07 -0300

    h10

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,73 @@
-import React from 'react';
+import React from "react";
 
-export default function  Form() {
+export function validate(input) {
+  //Username
+
+  let errors = {};
+  if (!input.username) {
+    errors.username = "Username is required";
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = "Username is invalid";
+  }
+
+  //Password
+  if (!input.password) {
+    errors.password = "Password is required";
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = "Password is invalid";
+  }
+
+  return errors;
+}
+
+export default function Form() {
+  // const [username, setUsername] = React.useState("");
+  // const [password, setPassword] = React.useState("");
+
+  const [input, setInput] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = React.useState({});
+
+  const handleInputChange = function (e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  setErrors(
+    validate({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
+  );
+
   return (
-      <div>
-        Componente Form
-      </div>
-  )
+    <form>
+      <label htmlFor="username">Username:</label>
+      <input
+        className={errors.username && "danger"}
+        type="text"
+        name="username"
+        id="username"
+        value={username}
+        onChange={handleInputChange}
+      />{" "}
+      {errors.username && <p className="danger">{errors.username}</p>}
+      <label htmlFor="password">Password:</label>
+      <input
+        className={errors.password && "danger"}
+        type="text"
+        name="password"
+        id="password"
+        value={password}
+        onChange={handleInputChange}
+      />
+      {errors.password && <p className="danger">{errors.password}</p>}
+      <input type="submit" />
+    </form>
+  );
 }
